a0813b2a9161189d1c37bdb8d46291c66802bf21
Jalend15 <jalend.bantupalli@gmail.com>
2024-05-19 19:27:14 -0700

    encode image function in raclip added with a small bug fix

--- vlm/raclip_vanilla.py
+++ vlm/raclip_vanilla.py
@@ -8,6 +8,7 @@ class RaClipVanilla(BaseVLM):
     # Should have all necessary components including clip, reference dataset, k etc..
     def __init__(self, name='RaClip Vanilla'):
         super().__init__(name)
+        self.model, self.preprocess = clip.load('ViT-B/32', device)
 
     # Should load and create the reference set
     def load_reference_set(self, dataset):
@@ -15,7 +16,9 @@ class RaClipVanilla(BaseVLM):
 
     # should implement retrieval from reference set and augmentation  
     def encode_image(self, image_batch):
-        pass
+        image_inputs = torch.stack([self.preprocess(image).unsqueeze(0) for image in image_batch]).to(device)
+        image_features = self.model.encode_image(image_inputs)
+        return image_features
 
     # same as clip 
     def encode_text(self, text_batch):
