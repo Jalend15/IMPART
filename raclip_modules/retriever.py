94ee00b0d72f83e9fcaa0dcc2f1b941425596e75
M Veeramakali Vignesh <veeramakali1@gmail.com>
2024-05-20 22:33:59 -0700

    modified raclip for zeroshot

--- raclip_modules/retriever.py
+++ raclip_modules/retriever.py
@@ -17,21 +17,6 @@ class Retriever:
         self.reference_set = []
         for x in self.reference_embeddings:
             self.reference_set.append([x['image_embedding'],x['text_embedding']])
-    def encode_image(self, image_batch):
-        image_inputs = torch.stack([self.preprocess(image).unsqueeze(0) for image in image_batch]).to(self.device).float()
-        print("Image inputs shape:", image_inputs.shape)  # Debug: Check the input shape
-        if image_inputs.dim() == 5:  # Checks if there's an extra batch dimension
-            image_inputs = image_inputs.squeeze(1)  # Remove the unnecessary dimension
-            print("Image inputs shape:", image_inputs.shape)
-
-        image_features = self.model.encode_image(image_inputs)
-        return image_features
-
-    def encode_text(self, text_batch):
-        text_inputs = torch.cat([clip.tokenize(text) for text in text_batch]).to(self.device)
-        text_features = self.model.encode_text(text_inputs)
-        return text_features
-    # returns k pairs of image, text embeddings from the vector db
     
     def load_embeddings(self, embeddings_path):
         """Load embeddings from a pickle file."""
@@ -40,17 +25,16 @@ class Retriever:
         print("Embeddings have been loaded from a pickle file.")
         return embeddings
 
-    def retrieve_similar(self, image_batch, top_k=5):
-        input_embedding = self.encode_image(image_batch)
-        similarities = [torch.cosine_similarity(input_embedding, ref_emb['image_embedding'], dim=1).item() for ref_emb in self.reference_embeddings]
+    def retrieve_similar(self, image_embeddings, top_k=5):
+        similarities = [torch.cosine_similarity(image_embeddings, ref_emb['image_embedding'], dim=1).item() for ref_emb in self.reference_embeddings]
         top_indices = np.argsort(similarities)[-top_k:][::-1]
         top_embeddings = torch.stack([self.image_embeddings[i] for i in top_indices])
         top_text_embeddings = torch.stack([self.text_embeddings[i] for i in top_indices])
 
         return top_embeddings, top_text_embeddings
-        return [(self.reference_set[i][0], self.reference_set[i][1], similarities[i]) for i in top_indices]
     
-retriever = Retriever()
-image_path = "./data/reference_set/" + "dog.jpg"
-image = Image.open(image_path).convert("RGB")
-print(retriever.retrieve_similar(image_batch=[image]))
+# retriever = Retriever()
+# image_path = "./data/reference_set/" + "dog.jpg"
+# image = Image.open(image_path).convert("RGB")
+# image_input = retriever.preprocess(image).unsqueeze(0)
+# print(retriever.retrieve_similar(retriever.model.encode_image(image_input)))
--- vlm/raclip_vanilla.py
+++ vlm/raclip_vanilla.py
@@ -1,10 +1,7 @@
 import torch
-import os
 import clip
-from base_vlm import BaseVLM
-import csv
+from vlm.base_vlm import BaseVLM
 from PIL import Image
-import numpy as np
 import raclip_modules.retriever as retriever
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -19,24 +16,17 @@ class RaClipVanilla(BaseVLM):
 
     # should implement retrieval from reference set and augmentation  
     def encode_image(self, image_batch):
-        image_inputs = torch.stack([self.preprocess(image).unsqueeze(0) for image in image_batch]).to(device).float()
-        print("Image inputs shape:", image_inputs.shape)  # Debug: Check the input shape
-        if image_inputs.dim() == 5:  # Checks if there's an extra batch dimension
-            image_inputs = image_inputs.squeeze(1)  # Remove the unnecessary dimension
-            print("Image inputs shape:", image_inputs.shape)
-
-        image_features = self.model.encode_image(image_inputs)
-        return image_features
+        image_features = self.model.encode_image(image_batch)
+        augmented_embedding = self.augment_image_embedding(image_features, 2)
+        return augmented_embedding
 
     def encode_text(self, text_batch):
         text_inputs = torch.cat([clip.tokenize(text) for text in text_batch]).to(device)
         text_features = self.model.encode_text(text_inputs)
         return text_features
 
-    def augment_image_embedding(self, input_image_paths, top_k=5, weights = [1.0,0.5,0.5]):
-        input_batch = [Image.open(image_path).convert("RGB") for image_path in input_image_paths]
-        input_embedding = self.encode_image(input_batch)
-        top_image_embeddings, top_text_embeddings = self.retriever.retrieve_similar(image_batch=input_batch)
+    def augment_image_embedding(self, input_embedding, top_k=5, weights = [1.0,0.005,0.005]):
+        top_image_embeddings, top_text_embeddings = self.retriever.retrieve_similar(input_embedding)
         # Combine embeddings with weights
         weighted_input_embedding = weights[0] * input_embedding
         weighted_image_embeddings = weights[1] * top_image_embeddings
@@ -45,12 +35,11 @@ class RaClipVanilla(BaseVLM):
         # Concatenate all embeddings and compute the weighted average
         combined_embedding = torch.cat((weighted_input_embedding, weighted_image_embeddings, weighted_text_embeddings), dim=0)
         augmented_embedding = torch.sum(combined_embedding, dim=0) / sum(weights)
-        print(input_embedding.shape)
-        print(augmented_embedding.shape)
 
-        return augmented_embedding
+        return augmented_embedding.unsqueeze(0)
     
-model = RaClipVanilla()
-image_path = "./data/reference_set/" + "dog.jpg"
-image = Image.open(image_path).convert("RGB")
-print(model.augment_image_embedding([image_path],2))
+# model = RaClipVanilla()
+# image_path = "./data/reference_set/" + "dog.jpg"
+# image = Image.open(image_path).convert("RGB")
+# image_input = model.preprocess(image).unsqueeze(0)
+# print(model.encode_image(image_input))
